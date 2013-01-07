0bf163fb6def513d731f42fa09a9fbeaa03121b7
Mario Pareja <pareja.mario@gmail.com>
2013-01-07 15:57:07 -0500

    Prompt for name and email, don't assume gvim.

--- configure_git_defaults.sh
+++ configure_git_defaults.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 echo Configuring git defaults
-git config --global user.name "Mario Pareja"
-git config --global user.email "pareja.mario@gmail.com"
+
+read -p 'Enter Full Name: ' FullName
+read -p 'Enter Email Address: ' Email
+
+git config --global user.name "$FullName"
+git config --global user.email "$Email"
 git config --global core.autocrlf false
-git config --global core.editor vim
+git config --global core.editor notepad
 git config --global core.pager "less -q -x4"
 git config --global color.ui auto
 git config --global log.date relative
@@ -13,9 +17,9 @@ git config --global push.default upstream
 git config --global alias.ca "commit --amend -C HEAD"
 
 # Borrowed from: http://www.jukie.net/bart/blog/pimping-out-git-log
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold cyan)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 
-git config --global guitool."edit file".cmd 'gvim "$FILENAME"'
+git config --global guitool."edit file".cmd 'notepad "$FILENAME"'
 git config --global guitool."edit file".noconsole yes
 git config --global guitool."delete file".cmd 'rm $FILENAME'
 git config --global guitool."delete file".noconsole yes
@@ -24,6 +28,9 @@ git config --global guitool.difftool.cmd 'git difftool -- "$FILENAME"'
 git config --global guitool.difftool.noconsole yes
 git config --global guitool."difftool staged".cmd 'git difftool --staged -- "$FILENAME"'
 git config --global guitool."difftool staged".noconsole yes
+
+echo Using default credential store.
+git config --global credential.helper store
 
 if [ -e /c ]; then
   echo "Using DiffMerge for windows"
